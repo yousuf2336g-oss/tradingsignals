7ba391f4052fbc3b9b2b01233cfe09f5d5a39451
yousuf2336g-oss <yousuf2336g@gmail.com>
2025-10-31 09:43:03 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+import requests
+from flask import Flask, request
+
+app = Flask(__name__)
+
+TELEGRAM_TOKEN = "8386452477:AAF2GhicIHYtZsqHa7dmahspE
+cvt1YCbMN4"
+CHAT_ID = "7123799580"
+
+@app.route('/signal', methods=['POST'])
+def signal():
+    data = request.json
+    message = data.get('message', '🚨 New Trading Signal!')
+    requests.get(f'https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage',
+                 params={'chat_id': CHAT_ID, 'text': message})
+    return 'ok', 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
